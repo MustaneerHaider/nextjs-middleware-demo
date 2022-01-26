c1de8e93734d4e1bfd0b9102620fa3d737b5498a
fahadtariq <abdullahtariqfahad@gmail.com>
2022-01-26 15:51:31 +0500

    db conn secured

--- util/db.js
+++ util/db.js
@@ -1,7 +1,5 @@
 import { MongoClient } from 'mongodb';
 
 export function connectToDatabase() {
-	return MongoClient.connect(
-		'mongodb+srv://admin_node:KvAZASexW7Tiejjr@cluster0.n1veg.mongodb.net/next?retryWrites=true&w=majority'
-	);
+	return MongoClient.connect(process.env.MONGO_URI);
 }
